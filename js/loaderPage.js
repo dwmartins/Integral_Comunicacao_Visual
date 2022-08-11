102ddbac14740060a0f4bf3d780108ead935c763
Douglas Wellington Martins <douglas5422@outlook.com>
2022-08-11 18:01:17 -0300

    Adcionado um spineer até a pagina carregar

---
+++ js/loaderPage.js
@@ -0,0 +1,7 @@
+var loader = document.querySelector('.spinner');
+
+window.addEventListener('load', function() {
+    loader.style.display = 'none';
+})
+
+//Nesta função vai carregar o Spinner na tela até a pagina estiver carregada!
